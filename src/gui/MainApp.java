508d9a76cf87775361944c6f2ab39248aafa1b74
dmvstar <dmvstar@gmail.com>
2017-09-11 16:01:22 +0300

    update file report

--- src/gui/MainApp.java
+++ src/gui/MainApp.java
@@ -11,9 +11,13 @@ import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
 
 public class MainApp {
 
@@ -50,39 +54,44 @@ public class MainApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 493, 273);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panel_main = new JPanel();
 		frame.getContentPane().add(panel_main, BorderLayout.CENTER);
 		SpringLayout sl_panel_main = new SpringLayout();
 		panel_main.setLayout(sl_panel_main);
-		
+
 		JLabel lblNewLabel = new JLabel("Path to data:");
 		sl_panel_main.putConstraint(SpringLayout.NORTH, lblNewLabel, 25, SpringLayout.NORTH, panel_main);
 		sl_panel_main.putConstraint(SpringLayout.WEST, lblNewLabel, 21, SpringLayout.WEST, panel_main);
 		panel_main.add(lblNewLabel);
-		
+
 		tfDataPath = new JTextField();
 		panel_main.add(tfDataPath);
 		tfDataPath.setColumns(10);
-		
+
 		JButton btnSelectData = new JButton("...");
+		btnSelectData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectDataSourceDir();
+			}
+		});
 		sl_panel_main.putConstraint(SpringLayout.EAST, tfDataPath, -16, SpringLayout.WEST, btnSelectData);
 		sl_panel_main.putConstraint(SpringLayout.NORTH, btnSelectData, -5, SpringLayout.NORTH, lblNewLabel);
 		sl_panel_main.putConstraint(SpringLayout.WEST, btnSelectData, -59, SpringLayout.EAST, panel_main);
 		sl_panel_main.putConstraint(SpringLayout.EAST, btnSelectData, -10, SpringLayout.EAST, panel_main);
 		panel_main.add(btnSelectData);
-		
+
 		JLabel lblReportTemplate = new JLabel("Report template:");
 		sl_panel_main.putConstraint(SpringLayout.NORTH, lblReportTemplate, 6, SpringLayout.SOUTH, lblNewLabel);
 		sl_panel_main.putConstraint(SpringLayout.WEST, lblReportTemplate, 0, SpringLayout.WEST, lblNewLabel);
 		panel_main.add(lblReportTemplate);
-		
+
 		JButton btnProcess = new JButton("Process");
 		sl_panel_main.putConstraint(SpringLayout.WEST, btnProcess, 0, SpringLayout.WEST, lblNewLabel);
 		sl_panel_main.putConstraint(SpringLayout.SOUTH, btnProcess, -10, SpringLayout.SOUTH, panel_main);
 		sl_panel_main.putConstraint(SpringLayout.EAST, btnProcess, 111, SpringLayout.WEST, panel_main);
 		panel_main.add(btnProcess);
-		
+
 		JComboBox cbTemplate = new JComboBox();
 		sl_panel_main.putConstraint(SpringLayout.WEST, tfDataPath, 0, SpringLayout.WEST, cbTemplate);
 		sl_panel_main.putConstraint(SpringLayout.SOUTH, tfDataPath, -6, SpringLayout.NORTH, cbTemplate);
@@ -90,33 +99,55 @@ public class MainApp {
 		sl_panel_main.putConstraint(SpringLayout.WEST, cbTemplate, 16, SpringLayout.EAST, lblReportTemplate);
 		sl_panel_main.putConstraint(SpringLayout.EAST, cbTemplate, -75, SpringLayout.EAST, panel_main);
 		panel_main.add(cbTemplate);
-		
+
 		JButton btnSelectTemplate = new JButton("...");
 		sl_panel_main.putConstraint(SpringLayout.NORTH, btnSelectTemplate, 6, SpringLayout.SOUTH, btnSelectData);
 		sl_panel_main.putConstraint(SpringLayout.WEST, btnSelectTemplate, 0, SpringLayout.WEST, btnSelectData);
 		sl_panel_main.putConstraint(SpringLayout.EAST, btnSelectTemplate, 0, SpringLayout.EAST, btnSelectData);
 		panel_main.add(btnSelectTemplate);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		sl_panel_main.putConstraint(SpringLayout.NORTH, menuBar, 0, SpringLayout.NORTH, panel_main);
 		sl_panel_main.putConstraint(SpringLayout.WEST, menuBar, 0, SpringLayout.WEST, panel_main);
 		sl_panel_main.putConstraint(SpringLayout.EAST, menuBar, 97, SpringLayout.WEST, panel_main);
 		panel_main.add(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+			}
+		});
 		mnFile.add(mntmExit);
-		
+
 		JPanel panel_down = new JPanel();
 		frame.getContentPane().add(panel_down, BorderLayout.SOUTH);
 		panel_down.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+			}
+		});
 		panel_down.add(btnCancel);
-		
+
 		JPanel panel_right = new JPanel();
 		frame.getContentPane().add(panel_right, BorderLayout.EAST);
+	}
+
+	protected void selectDataSourceDir() {
+		int ret = -1;
+		System.err.println("selectDataSourceDir");
+		JFileChooser fc = new JFileChooser();
+		ret = fc.showDialog(frame, "Choose file...");
+		if (ret == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			System.err.println(file);
+			tfDataPath.setText(file.getAbsolutePath());
+		}
 	}
 }
